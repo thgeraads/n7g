32e6d0b11288cf28839a53d1981a91945623f97c
thgeraads <theike2@gmail.com>
2025-06-13 15:18:00 +0200

    added .ISPW loading and unpacking

---
+++ n7g_theme_previewer/js/json.js
@@ -0,0 +1,81 @@
+export const json = {
+    "app_icons": [{"id": "229442202_1888", "name": "Music"}, {
+        "id": "229442211_1888",
+        "name": "Video"
+    }, {"id": "229442206_1888", "name": "Nike"}, {
+        "id": "229442203_1888",
+        "name": "Podcasts"
+    }, {"id": "229442204_1888", "name": "Photo"}, {
+        "id": "229442208_1888",
+        "name": "Radio"
+    }, {"id": "229442205_1888", "name": "Settings"}, {
+        "id": "229442209_1888",
+        "name": "Clock"
+    }, {"id": "229442207_1888", "name": "Voice recorder", "notes": "unused"}, {
+        "id": "229442210_1888",
+        "name": "iTunes U",
+        "notes": "unused"
+    }, {"id": "229442201_1888", "name": "Book", "notes": "unused"}],
+    "ui_elements": [{"id": "229442200_0064", "name": "App icon click shadow"}, {
+        "id": "229442213_0064",
+        "name": "Boot apple logo?"
+    }],
+    "wallpapers_by_color": {
+        "Slate": {
+            "solid": {"full_res": "229442215_0064", "preview": "229442228_0064"},
+            "striped": {"full_res": "229442241_0064", "preview": "229442254_0064"},
+            "dotted": {"full_res": "229442267_0064", "preview": "229442280_0064"},
+            "circles": {"full_res": "229442293_0064", "preview": "229442306_0064"},
+            "neutral": {"full_res": "229442216_0064", "preview": "229442229_0064"}
+        },
+        "Silver": {
+            "solid": {"full_res": "229442216_0064", "preview": "229442229_0064"},
+            "striped": {"full_res": "229442242_0064", "preview": "229442255_0064"},
+            "dotted": {"full_res": "229442268_0064", "preview": "229442281_0064"},
+            "circles": {"full_res": "229442294_0064", "preview": "229442307_0064"},
+            "neutral": {"full_res": "229442216_0064", "preview": "229442229_0064"}
+        },
+        "Blue": {
+            "solid": {"full_res": "229442217_0064", "preview": "229442230_0064"},
+            "striped": {"full_res": "229442243_0064", "preview": "229442256_0064"},
+            "dotted": {"full_res": "229442269_0065  ", "preview": "229442282_0065"},
+            "circles": {"full_res": "229442295_0065", "preview": "229442308_0065"},
+            "neutral": {"full_res": "229442216_0064", "preview": "229442229_0064"}
+        },
+        "Green": {
+            "solid": {"full_res": "229442218_0065", "preview": "229442231_0064"},
+            "striped": {"full_res": "229442244_0064", "preview": "229442257_0064"},
+            "dotted": {"full_res": "229442270_0065", "preview": "229442283_0065"},
+            "circles": {"full_res": "229442296_0065", "preview": "229442309_0065"},
+            "neutral": {"full_res": "229442216_0064", "preview": "229442229_0064"}
+        },
+        "Yellow": {
+            "solid": {"full_res": "229442219_0064", "preview": "229442232_0064"},
+            "striped": {"full_res": "229442245_0064", "preview": "229442258_0064"},
+            "dotted": {"full_res": "229442271_0065", "preview": "229442284_0065"},
+            "circles": {"full_res": "229442297_0065", "preview": "229442310_0065"},
+            "neutral": {"full_res": "229442216_0064", "preview": "229442229_0064"}
+        },
+        "Pink": {
+            "solid": {"full_res": "229442220_0064", "preview": "229442233_0064"},
+            "striped": {"full_res": "229442246_0065", "preview": "229442259_0064"},
+            "dotted": {"full_res": "229442272_0065", "preview": "229442285_0065"},
+            "circles": {"full_res": "229442298_0065", "preview": "229442311_0065"},
+            "neutral": {"full_res": "229442216_0064", "preview": "229442229_0064"}
+        },
+        "Purple": {
+            "solid": {"full_res": "229442221_0064", "preview": "229442234_0064"},
+            "striped": {"full_res": "229442247_0064", "preview": "229442260_0064"},
+            "dotted": {"full_res": "229442273_0065", "preview": "229442286_0065"},
+            "circles": {"full_res": "229442299_0065", "preview": "229442312_0064"},
+            "neutral": {"full_res": "229442216_0064", "preview": "229442229_0064"}
+        },
+        "Red": {
+            "solid": {"full_res": "229442222_0064", "preview": "229442235_0064"},
+            "striped": {"full_res": "229442248_0064", "preview": "229442261_0064"},
+            "dotted": {"full_res": "229442274_0065", "preview": "229442287_0065"},
+            "circles": {"full_res": "229442300_0065", "preview": "229442313_0065"},
+            "neutral": {"full_res": "229442216_0064", "preview": "229442229_0064"}
+        }
+    }
+}
